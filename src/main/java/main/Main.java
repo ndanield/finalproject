3cb1f69190d20a44be85ab9b72269b4572f6ef48
Carlos <carlosperezurena@gmail.com>
2018-08-04 00:45:27 -0400

    Revert "Configurando heroku"
    This reverts commit 52fd72bdc0d77242eb88597dc80a4f0ae252ac41.

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -45,7 +45,6 @@ public class Main {
         // Launch Database
         BootStrapServices.getInstance().init();
 
-        port(getHerokuAssignedPort());
         // Launch SOAPServices
 //        try{
 //            SoapMain.init();
@@ -332,12 +331,5 @@ public class Main {
         return null;
     }
 
-    private static int getHerokuAssignedPort(){
-        ProcessBuilder processBuilder = new ProcessBuilder();
-        if(processBuilder.environment().get("PORT") != null){
-            return Integer.parseInt(processBuilder.environment().get("PORT"));
-        }
-        return 4567;
-    }
 }
 
