52fd72bdc0d77242eb88597dc80a4f0ae252ac41
Carlos <carlosperezurena@gmail.com>
2018-08-04 00:41:51 -0400

    Configurando heroku

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -45,6 +45,7 @@ public class Main {
         // Launch Database
         BootStrapServices.getInstance().init();
 
+        port(getHerokuAssignedPort());
         // Launch SOAPServices
 //        try{
 //            SoapMain.init();
@@ -331,5 +332,12 @@ public class Main {
         return null;
     }
 
+    private static int getHerokuAssignedPort(){
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if(processBuilder.environment().get("PORT") != null){
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567;
+    }
 }
 
